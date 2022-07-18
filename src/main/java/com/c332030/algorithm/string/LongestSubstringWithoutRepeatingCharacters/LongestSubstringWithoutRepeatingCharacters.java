597aadf3815b33336ba2d87b46a22f4b61567843
c332030 <c332030@gmail.com>
2022-07-18 21:25:25 +0800

    Revert "feat: 调整"
    This reverts commit 787336a1fde825455d162007ecc1706e2764d36d.

--- src/main/java/com/c332030/algorithm/string/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/c332030/algorithm/string/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,8 +1,5 @@
 package com.c332030.algorithm.string.LongestSubstringWithoutRepeatingCharacters;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <p>
  * Description: LongestSubstringWithoutRepeatingCharacters 最长不重复子串
@@ -35,38 +32,4 @@ public class LongestSubstringWithoutRepeatingCharacters {
         System.out.println(result);
     }
 
-}
-
-class Solution {
-
-    /**
-     *
-     * @param s string字符串
-     * @return int整型
-     */
-    public int lengthOfLongestSubstring (String s) {
-
-        if(null == s || s.length() == 0) {
-            return 0;
-        }
-
-        int len = 0, strLen = s.length();
-        int start = 0,end = 0;
-
-        Map<Character, Integer> map = new HashMap<>(strLen);
-        while(end < strLen) {
-
-            char ch = s.charAt(end);
-            Integer index = map.get(ch);
-
-            if(null != index && index >= start) {
-                len = Math.max(len, end - start);
-                start = index + 1;
-            }
-
-            map.put(ch, end++);
-        }
-
-        return len;
-    }
 }
